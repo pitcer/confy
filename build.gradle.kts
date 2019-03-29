6523c947a09c77eda28aaded05758a889381d4a4
Piotr Dobiech <pitcer@protonmail.com>
2019-03-29 17:59:38 +0100

    Add junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,17 @@ repositories {
 
 dependencies {
 	implementation("com.typesafe:config:1.3.3")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.1")
+	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.1")
 }
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_11
 	targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform {
+		includeEngines("junit-jupiter")
+	}
 }
