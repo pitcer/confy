481081718de01517d918c6393e9ea74d56ec74aa
Piotr Dobiech <pitcer@protonmail.com>
2019-04-13 01:36:13 +0200

    Improve gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    java
+	`java-library`
 }
 
 repositories {
-    jcenter()
+	jcenter()
 }
 
 dependencies {
-	implementation("com.typesafe:config:1.3.3")
+	api("com.typesafe:config:1.3.3")
 	compileOnly("org.jetbrains:annotations:17.0.0")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.1")
-	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.1")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 }
 
 java {
@@ -18,8 +18,10 @@ java {
 	targetCompatibility = JavaVersion.VERSION_11
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform {
-		includeEngines("junit-jupiter")
+tasks {
+	test {
+		useJUnitPlatform {
+			includeEngines("junit-jupiter")
+		}
 	}
 }
