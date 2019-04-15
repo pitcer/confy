88d96bab9b43d5a6578b27d2d10cbb744c946dbf
Piotr Dobiech <pitcer@protonmail.com>
2019-04-15 03:27:17 +0200

    Add bintray plugin and setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
 	`java-library`
+	`maven-publish`
+	id("com.jfrog.bintray") version "1.8.4"
 }
 
 repositories {
@@ -16,6 +20,38 @@ dependencies {
 java {
 	sourceCompatibility = JavaVersion.VERSION_11
 	targetCompatibility = JavaVersion.VERSION_11
+}
+
+task<Jar>("sourcesJar") {
+	from(sourceSets["main"].allSource)
+	archiveClassifier.set("sources")
+}
+
+
+publishing {
+	publications {
+		create<MavenPublication>("maven") {
+			from(components["java"])
+			artifact(tasks["sourcesJar"])
+		}
+	}
+}
+
+bintray {
+	user = project.property("bintray-user") as String
+	key = project.property("bintray-api-key") as String
+	publish = true
+	setPublications("maven")
+	pkg(closureOf<BintrayExtension.PackageConfig> {
+		repo = project.property("bintray-repository") as String
+		name = project.name
+		desc = project.description
+		websiteUrl = project.property("vcs-url") as String
+		issueTrackerUrl = "$websiteUrl/issues"
+		vcsUrl = "$websiteUrl.git"
+		setLicenses(project.property("license") as String)
+		setLabels(*(project.property("labels") as String).split(",").toTypedArray())
+	})
 }
 
 tasks {
